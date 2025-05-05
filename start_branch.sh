9b81c5849e502ad5ed7bb013843812bfbca264d8
jenfransson <fransson.jen@gmail.com>
2025-05-05 09:08:11 +0200

    Allow other parents than main

--- start_branch.sh
+++ start_branch.sh
@@ -1,13 +1,22 @@
 #!bash
 
+cur=$(git symbolic-ref --short HEAD)
+
 git checkout $1 2>/dev/null || git checkout -b $1
 
 
 Rscript -e 'library(targets);tar_config_set(script = "_targets.R", store = "_targets_'$1'", project = "'$1'")'
 
-sed -i '' 's/TAR_PROJECT = "main"/TAR_PROJECT = "'$1'"/g' report.qmd
+sed -i '' 's/TAR_PROJECT = "'$cur'"/TAR_PROJECT = "'$1'"/g' report.qmd
+
+if [[ "$cur" == "main" ]]; then
+  par_tar='_targets'
+else
+  par_tar='_targets_'$cur
+fi
 
 
-rsync -av -f"+ */" -f"- *" _targets/ _targets_$1/
+rsync -av -f"+ */" -f"- *" $par_tar/ _targets_$1/
+
 
 
