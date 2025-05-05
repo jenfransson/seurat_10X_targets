7ddb77817a948ed95a06d9803f20ecc8e8a82b47
jenfransson <fransson.jen@gmail.com>
2025-05-05 11:12:22 +0200

    Remove ln part

--- start_branch.sh
+++ start_branch.sh
@@ -9,16 +9,5 @@ Rscript -e 'library(targets);tar_config_set(script = "_targets.R", store = "_tar
 
 sed -i '' 's/TAR_PROJECT = "'$cur'"/TAR_PROJECT = "'$1'"/g' report.qmd
 
-if [[ "$cur" == "main" ]]; then
-  par_tar='_targets'
-else
-  par_tar='_targets_'$cur
-fi
-
-
-rsync -av -f"+ */" -f"- *" $par_tar/ _targets_$1/
-ln -s $par_tar/objects/* _targets_$1/objects/
-ln -s $par_tar/meta/* _targets_$1/meta/
-ln -s $par_tar/workspaces/* _targets_$1/workspaces/
 
 
